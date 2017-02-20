cc5d86b590b2661107368af786320b797206d8a3
abhibalachandra <abhibalachandra@gmail.com>
2017-02-20 07:36:47 +0530

    Add files via upload

---
+++ thingsspeak.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import httplib, urllib
+import time
+sleep = 15 
+key = 'UL55AG7WUGJGFNRX'  
+def upload():
+    while True:
+        temp = "34"
+        params = urllib.urlencode({'field1': temp, 'key':key }) 
+        headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+        conn = httplib.HTTPConnection("api.thingspeak.com:80")
+        try:
+            conn.request("POST", "/update", params, headers)
+            response = conn.getresponse()
+            print temp
+            print response.status, response.reason
+            data = response.read()
+            conn.close()
+        except:
+            print "connection failed"
+        break
+#sleep for desired amount of time
+if __name__ == "__main__":
+        while True:
+                upload()
+                time.sleep(sleep)
